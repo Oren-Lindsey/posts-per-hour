256a9a35721ca3771031cc5caf9f78f97b8fae04
Oren Lindsey <67929847+Oren-Lindsey@users.noreply.github.com>
2022-04-05 14:45:30 -0400

    better time zone name
    thanks to https://stackoverflow.com/a/34405528/17038856

--- script.js
+++ script.js
@@ -92,23 +92,8 @@ function addChart(labels, dataset, timeFormat) {
 }
 
 function getTimezoneName() {
-  const today = new Date();
-  const short = today.toLocaleDateString(undefined);
-  const full = today.toLocaleDateString(undefined, { timeZoneName: 'long' });
-
-  // Trying to remove date from the string in a locale-agnostic way
-  const shortIndex = full.indexOf(short);
-  if (shortIndex >= 0) {
-    const trimmed = full.substring(0, shortIndex) + full.substring(shortIndex + short.length);
-    
-    // by this time `trimmed` should be the timezone's name with some punctuation -
-    // trim it from both sides
-    return trimmed.replace(/^[\s,.\-:;]+|[\s,.\-:;]+$/g, '');
-
-  } else {
-    // in some magic case when short representation of date is not present in the long one, just return the long one as a fallback, since it should contain the timezone's name
-    return full;
-  }
+  var zone = new Date().toLocaleTimeString(undefined,{timeZoneName:'short'}).split(' ')[2]
+  return zone
 }
 
 updateChart()
